2f3d21466cb0b8a9ed66a9db8e80e2940874ab93
Peter Winskill <p.winskill@imperial.ac.uk>
2023-11-21 11:26:17 +0000

    Tidy inputs

--- app.R
+++ app.R
@@ -8,17 +8,27 @@ ui <- fluidPage(
   titlePanel("Data Frame Editor"),
   sidebarLayout(
     sidebarPanel(
-      selectInput("countrySelect", "Select pre-loaded country", choices = c("None", "AGO", "COD")),
-      fileInput('file1', 'Or upload an .rds sf spatial file',
-                accept = c(
-                  '.rds'
-                )),
-      
-      uiOutput("columnSelect")
+      selectInput(
+        inputId = "countrySelect",
+        label = "Select pre-loaded country",
+        choices = c("None", "AGO", "COD")
+      ),
+      fileInput(
+        inputId = 'file1',
+        label = "Or upload an .rds sf spatial file",
+        accept = c(".rds")
+      ),
+      uiOutput(
+        outputId = "columnSelect"
+      )
     ),
     mainPanel(
-      DTOutput("editableTable"),
-      leafletOutput("map")
+      DTOutput(
+        outputId = "editableTable"
+      ),
+      leafletOutput(
+        outputId = "map"
+      )
     )
   )
 )
@@ -28,9 +38,6 @@ ui <- fluidPage(
 # BACK END ---------------------------------------------------------------------
 server <- function(input, output, session) {
   
-  # Reactive value for storing the data frame
-  data <- reactiveVal()
-  # Initialize reactiveValues to store the data frame
   reactiveDF <- reactiveValues(spatial = NULL, table = NULL)
   
   # Observe file upload
@@ -54,17 +61,13 @@ server <- function(input, output, session) {
   })
   
   observeEvent(input$selectedColumn, {
-    df <- reactiveDF$spatial  # Assuming 'data()' is your uploaded data frame
+    df <- reactiveDF$spatial
     if (is.null(df)) return()
-    
     selectedColumn <- input$selectedColumn
     uniqueValues <- unique(df[[selectedColumn]])
     initialDF <- data.frame(Values = uniqueValues, Input = NA_real_)
     colnames(initialDF) <- c(selectedColumn, "Input")
-    # Initialize reactiveDF with initialDF
     reactiveDF$table <- initialDF
-    
-    
   })
   
   # Render editable table
@@ -81,26 +84,17 @@ server <- function(input, output, session) {
   # Update reactive data frame on table edit
   observeEvent(input$editableTable_cell_edit, {
     info <- input$editableTable_cell_edit
-    
-    # Retrieve the current data frame from reactiveDF
     currentDF <- reactiveDF$table
-    
-    # Update the value in the data frame based on the edited cell
     currentDF[info$row, info$col] <- info$value
-    
-    # Update the reactive data frame
     reactiveDF$table <- currentDF
   })
   
-  # Render Plot
+  # Render Map
   observe({
     output$map <- renderLeaflet({
       spatial_data <- reactiveDF$spatial
       table <- reactiveDF$table
-      # browser()
       if (is.null(spatial_data) | is.null(table) | all(is.na(table$Input))) return(NULL)
-      # browser()
-      
       leaflet_input <- dplyr::left_join(spatial_data, table)
       palette <- colorNumeric("Blues", domain = NULL)
       
@@ -121,7 +115,6 @@ server <- function(input, output, session) {
     })
   })
 }
-
 # ------------------------------------------------------------------------------
 
 # RUN --------------------------------------------------------------------------
